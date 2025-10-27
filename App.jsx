cf5474a5e323a1283ccca158780dddfcddb952df
Jasmine Cheema <cheemajasmine2004@gmail.com>
2025-10-26 21:51:25 -0600

    added TodoList and ToDoForm and made changes to app.js

---
+++ App.jsx
@@ -0,0 +1,32 @@
+/**
+ * My To Do List App
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet
+} from 'react-native';
+import ToDoList from './ToDoList';
+import ToDoForm from './ToDoForm';
+
+
+function App() {
+
+  return (
+
+    <SafeAreaView >
+      <ToDoList />
+      <ToDoForm />
+    </SafeAreaView>
+
+  );
+}
+
+const styles = StyleSheet.create({
+ 
+});
+
+export default App;
